548ce48f9a1966dcbbfc39e6bd46aa0af4a1a73a
Alex Krentsel <akrentsel@berkeley.edu>
2019-06-18 22:58:55 +0300

    updated params to be launchable

--- src/components/content/Welcome.js
+++ src/components/content/Welcome.js
@@ -3,6 +3,8 @@ import React from "react";
 const Welcome = () => (
     <article className="post">
       <h2> Welcome to my Website </h2>
+      <hr/>
+      <p>This website is currently under construction. Please check back later.</p>
     </article>
 );
 
