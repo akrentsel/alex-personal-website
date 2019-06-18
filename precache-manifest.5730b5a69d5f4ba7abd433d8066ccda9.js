e0979f90b24c94dc41a6d3e792206015b17cfa58
Alex Krentsel <akrentsel@berkeley.edu>
2019-06-18 22:26:51 +0300

    Updates

---
+++ precache-manifest.5730b5a69d5f4ba7abd433d8066ccda9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951dfc6a252c7be73848b394467bf335",
+    "url": "/alex-personal-website/index.html"
+  },
+  {
+    "revision": "17f51bab90297159f1c0",
+    "url": "/alex-personal-website/static/css/main.c3baa70f.chunk.css"
+  },
+  {
+    "revision": "7308c38b9ad69938fcad",
+    "url": "/alex-personal-website/static/js/2.672c19db.chunk.js"
+  },
+  {
+    "revision": "17f51bab90297159f1c0",
+    "url": "/alex-personal-website/static/js/main.0bb8198e.chunk.js"
+  },
+  {
+    "revision": "b023b4c7074cbe95f07b",
+    "url": "/alex-personal-website/static/js/runtime~main.a519a3f0.js"
+  },
+  {
+    "revision": "96add150472826acc90ab5bf7ab673ec",
+    "url": "/alex-personal-website/static/media/alex-nature.96add150.jpg"
+  },
+  {
+    "revision": "9f8e47a30debb17ee3d197f09800cc51",
+    "url": "/alex-personal-website/static/media/alex-normal.9f8e47a3.jpg"
+  },
+  {
+    "revision": "6db494f0a7787e1813227e7d79b9cdbb",
+    "url": "/alex-personal-website/static/media/alex-silly.6db494f0.jpg"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/alex-personal-website/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/alex-personal-website/static/media/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/alex-personal-website/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/alex-personal-website/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/alex-personal-website/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/alex-personal-website/static/media/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "/alex-personal-website/static/media/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/alex-personal-website/static/media/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/alex-personal-website/static/media/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/alex-personal-website/static/media/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/alex-personal-website/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/alex-personal-website/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/alex-personal-website/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/alex-personal-website/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/alex-personal-website/static/media/fa-solid-900.ef3df984.eot"
+  }
+]);
