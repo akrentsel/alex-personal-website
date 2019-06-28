4252601468db70a7d4849a4628de0132893c0077
Alex Krentsel <akrentsel@berkeley.edu>
2019-06-28 19:34:39 +0300

    filling in welcome page

--- src/components/content/Welcome.js
+++ src/components/content/Welcome.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Welcome = () => (
     <article className="post">
-      <h2> Welcome to my Website </h2>
-      <p>This website is currently under construction. Please check back later.</p>
+      <header>
+        <div className="title">
+          <h2>Welcome to my website</h2>
+        </div>
+      </header>
+      <p>Thanks for stopping by. This webpage is home to <Link to="/about">info about me and my life/career timeline</Link>, my <Link to="/music">violin performance recordings</Link>, <Link to="/teaching">teaching materials I've produced</Link>, and my <Link to="/contact">contact info</Link>.</p>
+      <p>Take a look around!</p>
     </article>
 );
+
+// TODO(akrentsel): Link to website source
 
 export default Welcome;
