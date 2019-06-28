5019a91770e855ebe81daff531f1b16e5843fe4f
Alex Krentsel <akrentsel@berkeley.edu>
2019-06-28 10:09:11 +0300

    made menu close on click

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,25 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import { slide as Menu } from 'react-burger-menu'
 
+import 'assets/css/navbar.css';
+
 
 class NavBar extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      menuOpen: false
+    }
+  }
+
+  handleStateChange (state) {
+    this.setState({menuOpen: state.isOpen})
+  }
+
+  closeMenu () {
+    this.setState({menuOpen: false})
+  }
+
   showSettings (event) {
     event.preventDefault();
   }
@@ -26,12 +43,17 @@ class NavBar extends React.Component {
 							<ul>
 								<li className="menu">
 									<a className="fa-bars">
-                    <Menu right width={200}>
-                      <Link to="/">Home</Link>
-                      <Link to="/about">About</Link>
-                      <Link to="/music">Music</Link>
-                      <Link to="/teaching">Teaching</Link>
-                      <Link to="/contact">Contact</Link>
+                    <Menu
+                      isOpen={this.state.menuOpen}
+                      onStateChange={(state) => this.handleStateChange(state)}
+                      right
+                      width={200}
+                    >
+                      <Link onClick={() => this.closeMenu()} to="/">Home</Link>
+                      <Link onClick={() => this.closeMenu()} to="/about">About</Link>
+                      <Link onClick={() => this.closeMenu()} to="/music">Music</Link>
+                      <Link onClick={() => this.closeMenu()} to="/teaching">Teaching</Link>
+                      <Link onClick={() => this.closeMenu()} to="/contact">Contact</Link>
                     </Menu>
                   </a>
 								</li>
