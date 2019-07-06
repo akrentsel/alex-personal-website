844ebb1646663eea0f87431eeae2e99eca53f6d7
Alex Krentsel <akrentsel@berkeley.edu>
2019-07-06 12:10:41 +0300

    GA started adding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+// import createBrowserHistory from 'history/createBrowserHistory';
+// import GoogleAnalytics from "react-ga";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import DocumentTitle from 'react-document-title';
 import ScrollToTop from 'react-router-scroll-top'
@@ -13,7 +15,15 @@ import Contact from './components/content/Contact.js';
 import Welcome from './components/content/Welcome.js';
 import BriefAboutInsert from './components/BriefAboutInsert.js';
 
+// GoogleAnalytics.initialize("UA-143326735-1");
+// var history = createBrowserHistory();
+// history.listen(function (location) {
+//     GoogleAnalytics.ga('set', 'page', location.pathname + location.search);
+//     GoogleAnalytics.ga('send', 'pageview');
+// });
+
 class App extends React.Component {
+
   render = () => (
     <DocumentTitle title='Alex Krentsel'>
       <Router>
