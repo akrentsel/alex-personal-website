0818f324e46151e4dfb8f23e0efd6c29a30416c9
Alex Krentsel <akrentsel@berkeley.edu>
2020-06-05 20:32:55 -0700

    done updating projects for now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from "react";
-import Analytics from 'react-router-ga';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import DocumentTitle from 'react-document-title';
-import ScrollToTop from 'react-router-scroll-top'
-import './assets/css/main.css';
+import Analytics from "react-router-ga";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import DocumentTitle from "react-document-title";
+import ScrollToTop from "react-router-scroll-top";
+import "./assets/css/main.css";
 
-import NavBar from './components/NavBar.js';
-import About from './components/content/About.js';
-import Music from './components/content/Music.js';
-import Teaching from './components/content/Teaching.js';
-import Projects from './components/content/Projects.js';
-import Contact from './components/content/Contact.js';
-import Welcome from './components/content/Welcome.js';
-import BriefAboutInsert from './components/BriefAboutInsert.js';
+import NavBar from "./components/NavBar.js";
+import About from "./components/content/About.js";
+import Music from "./components/content/Music.js";
+import Teaching from "./components/content/Teaching.js";
+import Projects from "./components/content/Projects.js";
+import Contact from "./components/content/Contact.js";
+import Welcome from "./components/content/Welcome.js";
+import BriefAboutInsert from "./components/BriefAboutInsert.js";
 
 class App extends React.Component {
   render = () => (
-    <DocumentTitle title='Alex Krentsel'>
+    <DocumentTitle title="Alex Krentsel">
       <Router>
         <Analytics id="UA-143332212-1">
-          <ScrollToTop/>
+          <ScrollToTop />
           <div id="wrapper">
             <Route path="/" component={NavBar} />
             <div id="main">
@@ -35,7 +35,7 @@ class App extends React.Component {
         </Analytics>
       </Router>
     </DocumentTitle>
-  )
+  );
 }
 
 export default App;
