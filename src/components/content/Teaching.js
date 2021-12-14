156bed1d29efda1ba7c8d30044a7b9a6a11c527c
Alex Krentsel <akrentsel@berkeley.edu>
2021-12-13 21:01:47 -0500

    added Howard and Stanford to my teaching experience

--- src/components/content/Teaching.js
+++ src/components/content/Teaching.js
@@ -15,6 +15,35 @@ const Teaching = () => (
             <h2>Teaching</h2>
           </div>
         </header>
+        <h3>Howard University</h3>
+        <p>
+          For Fall 2021, I was on faculty at <a target="_blank" rel="noopener noreferrer" href="https://www.howard.edu/">Howard University</a> as{" "}
+          a visitng lecturer teaching 185 students in CSCI100: Intro to Computer Science.{" "}
+          <ul>
+            <li>Developed <a target="_blank" rel="noopener noreferrer" href="https://www.csci100.org/calendar">syllabus and curriculum</a> based on industry standards and CS courses at Berkeley and Stanford.</li>
+            <li>Produced, recorded, and published <a target="_blank" rel="noopener noreferrer" href="https://www.csci100.org/">~125 lecture videos on course YouTube Channel</a>.</li>
+            <li>Built course website, <a target="_blank" rel="noopener noreferrer" href="https://www.csci100.org/">csci100.org</a>, to act as centralized public hub for students and allow for auditing the course.</li>
+            <li>Hired and managed team of 5 Howard TAs, hosting office hours and writing/grading assignments.</li>
+            <li>Recruited 15 volunteer TAs from Google, providing weekly 30 hours of office hours, 15 hours of mock interviews, 6 hours of 1:1 tutoring.</li>
+          </ul>
+        </p>
+        <article className="mini-post">
+          <header>
+            <h2>CSCI100 Fall 2021 Class Website</h2>
+            <time className="published" datetime="2021-08-15">Fall 2021</time>
+            <p>Course website I built to host all content for my course publicly,{" "}
+              including lecture videos, slides, topic calendar, syllabus, announcements, and more.
+            </p>
+          </header>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.csci100.org/calendar" className="image"><img height="400" src={EntropyExplanationPic} alt="" /></a>
+        </article>
+        <h3>Stanford University</h3>
+        <p>I've TA'ed Stanford's Code-in-Place course twice (Spring 2020, 2021),{" "}
+        as part of a 900 person teaching staff instructing 10,000 students around the world.{" "}
+        I taugtht a section of 10 students each week for 5 weeks. My mom wanted to learn to{" "}
+        code, so she took the course with me in Spring 2021.
+        </p>
+        <h3>UC Berkeley</h3>
         <p>While at UC Berkeley, I worked as a TA for 5 semesters, teaching
         <ul style={{listStylePosition: "inside"}}>
           <li><a target="_blank" rel="noopener noreferrer" href="https://inst.eecs.berkeley.edu/~ee16a/fa16/">EE16A: Introduction to Electrical Engineering</a>, Fall 2016</li>
