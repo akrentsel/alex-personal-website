5be39302c777cc489a6e8ab6dfaf20607b1c7b0e
Alex Krentsel <alex.krentsel@gmail.com>
2022-07-28 23:13:41 -0700

    fixed wording

--- src/components/content/Welcome.js
+++ src/components/content/Welcome.js
@@ -9,7 +9,7 @@ const Welcome = () => (
         <h2>Welcome to my website</h2>
       </div>
     </header>
-    <p>Thanks for stopping by internet home. Take a look around the tabs above. Or check out some of my featured posts below:</p>
+    <p>Thanks for stopping by internet home. Take a look around, or check out some of my featured posts below:</p>
     <PostList featured/>
   </article>
 );
